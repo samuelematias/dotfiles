38ca6eeb1d1983c7318ca0bc9bb1132ddb566b25
samuelematias <hi@samuelematias.com>
2021-01-03 13:34:37 -0300

    perf(ohmyzsh) improving ohmyzsh.sh

--- osx/tasks/ohmyzsh.sh
+++ osx/tasks/ohmyzsh.sh
@@ -1,3 +1,9 @@
 echo "⚖️ Cloning Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "✅ Successful cloned Oh My Zsh"
+echo "⚖️ Cloning powerlevel10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "✅ Successful cloned powerlevel10k"
+echo "⚖️ Running source .zshrc..."
+source ~/.zshrc
+echo "✅ Successful Runned source .zshrc"
