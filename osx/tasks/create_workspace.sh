6d961c1f120e0228bf011e2153fa0279c327fc27
Samuel Matias <hi@samuelematias.com>
2021-01-16 11:44:33 -0300

    feat(sh): add new folders to create

--- osx/tasks/create_workspace.sh
+++ osx/tasks/create_workspace.sh
@@ -3,3 +3,6 @@
 # Create a Workspace directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
 mkdir -p $HOME/workspace
+cd ~/workspace
+mkdir -p job
+mkdir -p personal
