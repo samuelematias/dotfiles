7811371730902f5d32cc5be002c08329b9965844
Samuel Matias <hi@samuelematias.com>
2021-02-11 18:24:36 -0300

    chore(emacs): update/add new doom files

---
+++ config_files/.config/doom/+bindings.el
@@ -0,0 +1,189 @@
+;;; ~/.doom.d/+bindings.el -*- lexical-binding: t; -*-
+
+(when (eq system-type 'gnu/linux)
+  (setq x-super-keysym 'meta))
+
+(define-key evil-normal-state-map (kbd "M-r") 'evil-multiedit-match-all)
+(define-key evil-normal-state-map (kbd "<tab>") 'evil-jump-item)
+
+(defun scroll-up-bottom-window ()
+  "Scroll up bottom window"
+  (interactive)
+  (save-selected-window
+    (when-let ((bottom-window (-first (lambda (w)
+                                        (and (window-full-width-p w)
+                                             (not (eq w (get-buffer-window))))) (window-list))))
+      (select-window bottom-window)
+      (scroll-up-line))))
+
+(defun scroll-down-bottom-window ()
+  "Scroll down bottom window"
+  (interactive)
+  (save-selected-window
+    (when-let ((bottom-window (-first (lambda (w)
+                                        (and (window-full-width-p w)
+                                             (not (eq w (get-buffer-window))))) (window-list))))
+      (select-window bottom-window)
+      (scroll-down-line))))
+
+(map! :n
+
+      :desc "Multi edit lines"
+      "M-r" #'evil-multiedit-match-all)
+
+(map! :nvi
+
+      :desc "Toggle buffer full screen"
+      "<f10>" #'doom/window-maximize-buffer
+
+      :desc "increase window width"
+      "C-S-<left>" (lambda () (interactive) (enlarge-window 5 t))
+
+      :desc "decrease window width"
+      "C-S-<right>" (lambda () (interactive) (enlarge-window -5 t))
+
+      :desc "increase window height"
+      "C-S-<down>" (lambda () (interactive) (enlarge-window 5))
+
+      :desc "decrease window height"
+      "C-S-<up>" (lambda () (interactive) (enlarge-window -5))
+
+      :desc "scroll up bottom window"
+      "C-S-M-<down>" #'scroll-up-bottom-window
+
+      :desc "scroll down bottom window"
+      "C-S-M-<up>" #'scroll-down-bottom-window
+
+      :desc "Expand region"
+      "M-=" #'er/expand-region
+
+      :desc "Reverse expand region"
+      "M--" (lambda () (interactive) (er/expand-region -1)))
+
+(map! :nv
+      :desc "Evil multiedit word"
+      "M-r" #'evil-multiedit-match-all)
+
+(map! :leader
+
+      :desc "Open dotfiles"
+      "f T" #'open-dotfiles
+
+      :desc "Find file in dotfiles"
+      "f t" #'find-in-dotfiles)
+
+(after! paredit
+  (define-key paredit-mode-map (kbd "C-<left>") nil)
+  (define-key paredit-mode-map (kbd "C-<right>") nil)
+
+  (map! :nvi
+
+        :desc "Forward barf"
+        "M-<left>" #'paredit-forward-barf-sexp
+
+        :desc "Forward slurp"
+        "M-<right>" #'paredit-forward-slurp-sexp
+
+        :desc "Backward slurp"
+        "M-S-<left>" #'paredit-backward-slurp-sexp
+
+        :desc "Backward barf"
+        "M-S-<right>" #'paredit-backward-barf-sexp
+
+        :desc "Reverse transpose sexps"
+        "M-<up>" #'reverse-transpose-sexps
+
+        :desc "Transpose sexps"
+        "M-<down>" #'transpose-sexps
+
+        :desc "Backward"
+        "C-c <left>" #'paredit-backward
+
+        :desc "Forward"
+        "C-c <right>" #'paredit-forward))
+
+(map! :after clojure-mode
+      :leader
+
+      :desc "Search for symbol in project excluding test folders"
+      "&" (lambda () (interactive) (rg-ignoring-folders (list "test" "postman")))
+
+      :desc "Search for symbol in project excluding src folder"
+      "(" (lambda () (interactive) (rg-ignoring-folders (list "src")))
+
+      :desc "lsp-clojure cursor info"
+      "-" #'lsp-clojure-cursor-info)
+
+(after! clj-refactor
+  (define-key clj-refactor-map "\C-ctf" #'cljr-thread-first-all)
+  (define-key clj-refactor-map "\C-ctl" #'cljr-thread-last-all)
+  (define-key clj-refactor-map "\C-cu" #'cljr-unwind)
+  (define-key clj-refactor-map "\C-cU" #'cljr-unwind-all))
+
+(map! :after dap-mode
+      :map dap-mode-map
+      :n
+
+      :desc "DAP step-in"
+      "<f4>" #'dap-step-in
+
+      :desc "DAP step-out"
+      "<f5>" #'dap-step-out
+
+      :desc "DAP next"
+      "<f6>" #'dap-next
+
+      :desc "DAP continue"
+      "<f8>" #'dap-continue)
+
+(after! lsp-ui
+  (map! :nvi
+
+        :desc "LSP-ui apply code action"
+        "M-[" #'lsp-ui-sideline-apply-code-actions))
+
+(map! :leader
+
+      :desc "LSP-treemacs list errors"
+      "c X" #'lsp-treemacs-errors-list
+
+      :desc "LSP-treemacs find references"
+      "c R" (lambda () (interactive) (lsp-treemacs-references t)))
+
+(map! :after java-mode
+      :map java-mode-map
+      :localleader
+
+      :desc "Run method test"
+      "t" #'dap-java-run-test-method
+
+      :desc "Run class tests"
+      "T" #'dap-java-run-test-class)
+
+(map! :after dart-mode
+      :map dart-mode-map
+      :localleader
+
+      :desc "Flutter run or hot reload"
+      "j" #'flutter-run-or-hot-reload
+
+      :desc "Hover run or hot reload"
+      "k" #'hover-run-or-hot-reload
+
+      :desc "Hover run or hot restart"
+      "K" #'hover-run-or-hot-restart
+
+      :desc "Hover clear buffer"
+      "c" #'hover-clear-buffer
+
+      :desc "Hover kill buffer"
+      "x" #'hover-kill
+
+      :desc "Run current file test"
+      "t" #'lsp-dart-run-test-file
+
+      :desc "Run test at point"
+      "T" #'lsp-dart-run-test-at-point
+
+      :desc "Run all tests"
+      "M-t" #'lsp-dart-run-all-tests)
--- config_files/.config/doom/config.el
+++ config_files/.config/doom/config.el
@@ -1,99 +1,231 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Samuel Matias"
-      user-mail-address "hi@samuelematias.com")
-
-;; (toggle-frame-maximized)
-;; (toggle-frame-fullscreen)
-
-;; (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
-;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-;; (when window-system
-;;   (set-frame-position (selected-frame) 330 88)
-;;   (set-frame-size (selected-frame) 184 54))
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-to-list 'auto-mode-alist '("\\.repl\\'" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.ect\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.xtend\\'" . java-mode))
+(add-hook 'html-mode-hook #'turn-off-auto-fill)
+(add-hook 'markdown-mode-hook #'turn-off-auto-fill)
+(add-hook 'dart-mode-hook (lambda () (setq left-fringe-width 16)))
+(add-hook 'java-mode-hook (lambda () (setq left-fringe-width 16)))
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(defun reverse-transpose-sexps (arg)
+    (interactive "*p")
+    (transpose-sexps (- arg))
+    (backward-sexp (1+ arg))
+    (forward-sexp 1))
 
-(setq doom-font (font-spec :family "Fira Code" :size 15)
-      doom-variable-pitch-font (font-spec :family "Fira Code" :size 15)
-      doom-big-font (font-spec :family "Fira Code" :size 24))
+(defun rg-ignoring-folders (folders)
+  "ripgrep selected word in project excluding folder"
+  (let ((symbol (thing-at-point 'symbol t))
+        (args (mapconcat 'identity
+                         (mapcar #'(lambda(folder) (concat "-g '!" folder "/*'"))
+                                 folders)
+                         " ")))
+    (counsel-rg symbol (counsel--git-root) args)))
+
+(setq-default evil-kill-on-visual-paste nil)
+
+(setq
+ history-length 300
+ indent-tabs-mode nil
+ confirm-kill-emacs nil
+ mode-line-default-help-echo nil
+ show-help-function nil
+ evil-multiedit-smart-match-boundaries nil
+ compilation-scroll-output 'first-error
+
+ read-process-output-max (* 1024 1024)
+
+ projectile-project-search-path '("~/workspace/" "~/workspace/job/" "~/workspace/personal/" "~/workspace/personal/flutter/" "~/workspace/job/ifood/")
+ projectile-enable-caching nil
+
+ evil-split-window-below t
+ evil-vsplit-window-right t
+
+ counsel-rg-base-command "rg -i -M 1000 --no-heading --line-number --color never %s ."
+
+ frame-title-format (setq icon-title-format  ;; set window title with "project"
+                          '((:eval (projectile-project-name))))
+
+ doom-font (font-spec :family "FiraCode Nerd Font" :size 16)
+ doom-unicode-font (font-spec :family "Material Design Icons")
+ doom-big-font-increment 2
+
+ doom-theme 'doom-dracula
+ doom-themes-treemacs-theme "all-the-icons"
+ doom-localleader-key ","
+
+ +format-on-save-enabled-modes '(dart-mode)
+ +lsp-auto-install-servers t
+
+ dap-enable-mouse-support nil
+
+ evil-collection-setup-minibuffer t)
 
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
-(setq doom-theme 'doom-dracula)
+(set-popup-rule! "\\*LSP Dart tests\\*" :height 0.3)
+(set-popup-rule! "\\*Hover\\*" :quit nil)
+(set-popup-rule! "\\*dap-ui-locals\\*" :side 'right :width 0.3)
+(set-popup-rule! "\\*dap-ui-sessions\\*" :side 'right :width 0.3)
+(set-popup-rule! "\\*midje-test-report\\*" :side 'right :width 0.5)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(use-package! cider
+  :after clojure-mode
+  :config
+  (setq cider-ns-refresh-show-log-buffer t
+        cider-show-error-buffer t;'only-in-repl
+        cider-font-lock-dynamically '(macro core function var deprecated)
+        cider-prompt-for-symbol nil)
+  (set-popup-rule! "*cider-test-report*" :side 'right :width 0.5)
+  (set-popup-rule! "^\\*cider-repl" :side 'bottom :quit nil)
+  (set-lookup-handlers! 'cider-mode nil))
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(use-package! clj-refactor
+  :after clojure-mode
+  :config
+  (set-lookup-handlers! 'clj-refactor-mode nil)
+  (setq cljr-warn-on-eval nil
+        cljr-eagerly-build-asts-on-startup nil
+        cljr-add-ns-to-blank-clj-files nil
+        cljr-magic-require-namespaces
+        '(("s"   . "schema.core")
+          ("th"  . "common-core.test-helpers")
+          ("gen" . "common-test.generators")
+          ("d-pro" . "common-datomic.protocols.datomic")
+          ("ex" . "common-core.exceptions")
+          ("dth" . "common-datomic.test-helpers")
+          ("t-money" . "common-core.types.money")
+          ("t-time" . "common-core.types.time")
+          ("d" . "datomic.api")
+          ("m" . "matcher-combinators.matchers")
+          ("pp" . "clojure.pprint"))))
+
+(use-package! clojure-mode
+  :config
+  (setq clojure-indent-style 'align-arguments
+        clojure-thread-all-but-last t
+        clojure-align-forms-automatically t
+        yas-minor-mode 1)
+  (cljr-add-keybindings-with-prefix "C-c C-c"))
+
+(use-package! company
+  :config
+  (setq company-tooltip-align-annotations t))
+
+(use-package! hover
+  :after dart-mode
+  :config
+  (setq hover-hot-reload-on-save t
+        hover-clear-buffer-on-hot-restart t
+        hover-screenshot-path "$HOME/Pictures"))
+
+(use-package! doom-modeline
+  :init
+  (setq doom-modeline-major-mode-icon t
+        doom-modeline-buffer-encoding nil
+        doom-modeline-buffer-file-name-style 'relative-to-project
+        doom-modeline-vcs-max-length 0
+        vc-display-status nil))
+
+(use-package! lsp-dart
+  :config
+  (when-let (dart-exec (executable-find "dart"))
+    (let ((dart-sdk-path (-> dart-exec
+                             file-chase-links
+                             file-name-directory
+                             directory-file-name
+                             file-name-directory)))
+      (setq lsp-dart-sdk-dir dart-sdk-path
+            lsp-dart-dap-flutter-hot-reload-on-save t))))
 
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;;(use-package! lsp-dart
+;;  :config
+;;  (when-let (dart-exec (executable-find "dart"))
+;;    (let ((dart-sdk-path (-> dart-exec
+;;                             file-chase-links
+;;                             file-name-directory
+;;                             directory-file-name
+;;                             file-name-directory)))
+;;     (setq lsp-dart-sdk-dir "/usr/local/Caskroom/flutter/1.22.5/flutter/bin/cache/dart-sdk"
+;;            lsp-dart-dap-flutter-hot-reload-on-save t))))
 
-;; Install use-package
-(condition-case nil
-    (require 'use-package)
-  (file-error
-   (require 'package)
-   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-   (package-initialize)
-   (package-refresh-contents)
-   (package-install 'use-package)
-   (require 'use-package)))
+;;(use-package lsp-dart
+;;  :ensure t
+;;  :hook (dart-mode . lsp))
+            
+;;(setq lsp-dart-sdk-dir "/usr/local/Caskroom/flutter/1.22.5/flutter/bin/cache/dart-sdk")
+;;(setq lsp-flutter-sdk-dir "/usr/local/Caskroom/flutter/1.22.5/flutter")
+;;(setq dart-server-sdk-path "/usr/local/Caskroom/flutter/1.22.5/flutter/bin/cache/dart-sdk")
+;;(setq dart-server-executable-path "/usr/local/Caskroom/flutter/1.22.5/flutter/bin/cache/dart-sdk")
+;;(setq dart-server-enable-analysis-server t)
+;;(add-hook 'dart-server-hook 'flycheck-mode)
 
-(use-package lsp-mode :ensure t)
-(use-package lsp-dart
-  :ensure t
-  :hook (dart-mode . lsp))
+(use-package vterm
+  :commands vterm
+  :config
+  (setq vterm-max-scrollback 10000))
 
-;; Optional packages
-(use-package projectile :ensure t) ;; project management
-(use-package yasnippet
-  :ensure t
-  :config (yas-global-mode)) ;; snipets
-(use-package lsp-ui :ensure t) ;; UI for LSP
-(use-package company :ensure t) ;; Auto-complete
+(use-package! lsp-java
+  :after lsp
+  :init
+  (setq lsp-java-format-settings-url "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml"
+        lsp-java-format-settings-profile "GoogleStyle"
+        lsp-java-save-actions-organize-imports t
+        lsp-java-references-code-lens-enabled t
+        lsp-java-implementations-code-lens-enabled t
+        lsp-file-watch-ignored-directories
+        '(".idea" ".ensime_cache" ".eunit" "node_modules"
+          ".git" ".hg" ".fslckout" "_FOSSIL_"
+          ".bzr" "_darcs" ".tox" ".svn" ".stack-work"
+          "build")))
 
-;; Optional Flutter packages
-(use-package hover :ensure t) ;; run app from desktop without emulator
+(use-package! lsp-mode
+  :commands lsp
+  :hook ((clojure-mode . lsp)
+         (dart-mode . lsp)
+         (java-mode . lsp))
+  :config
+    (setq lsp-clojure-custom-server-command t
+          lsp-headerline-breadcrumb-enable t
+          lsp-lens-enable t
+          lsp-enable-file-watchers t
+          lsp-signature-auto-activate nil
+          lsp-semantic-tokens-enable t
+          lsp-completion-use-last-result nil))
+  (advice-add #'lsp-rename :after (lambda (&rest _) (projectile-save-project-buffers)))
+
+(use-package! lsp-ui
+  :after lsp-mode
+  :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-peek-list-width 60
+        lsp-ui-peek-fontify 'always
+        lsp-ui-sideline-show-code-actions nil))
+
+(use-package! org-tree-slide
+  :config
+  (setq +org-present-text-scale 2
+        org-tree-slide-modeline-display 'outside
+        org-tree-slide-fold-subtrees-skipped nil)
+  (add-hook! 'org-tree-slide-play-hook #'org-display-inline-images)
+  (add-hook! 'org-tree-slide-play-hook #'doom-disable-line-numbers-h))
+
+(use-package! paredit
+  :hook ((clojure-mode . paredit-mode)
+         (emacs-lisp-mode . paredit-mode)))
+
+(use-package! dap-mode
+  :init
+  (require 'dap-chrome))
+
+(use-package! treemacs-all-the-icons
+  :after treemacs)
+
+(after! projectile
+  (add-to-list 'projectile-project-root-files-bottom-up "pubspec.yaml")
+  (add-to-list 'projectile-project-root-files-bottom-up "BUILD"))
+
+(put 'narrow-to-region 'disabled nil)
