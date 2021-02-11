385c54a15fb606ce311573ae27efee19cb04f293
Samuel Matias <hi@samuelematias.com>
2021-02-11 18:34:08 -0300

    chore(dock): add emacs on dock

--- osx/tasks/dock.sh
+++ osx/tasks/dock.sh
@@ -8,6 +8,7 @@ disable_recent_apps_from_dock
 
 add_app_to_dock "Vivaldi"
 add_app_to_dock "Alacritty"
+add_app_to_dock "Emacs"
 add_app_to_dock "Notion"
 add_app_to_dock "Spotify"
 add_folder_to_dock $HOME/Downloads -d 0 -s 2 -v 1
